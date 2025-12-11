b9e0291e6e2a007a35a184ce4aa2f1496d26ebdd
Alexandre Mary <amary@f0r7s18.clusters.42paris.fr>
2025-12-11 20:28:31 +0100

    push_swap

---
+++ push_swap.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amary <amary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 15:07:19 by amary             #+#    #+#             */
+/*   Updated: 2025/12/10 19:51:57 by amary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+
+#include "ft_printf.h"
+#include <stdlib.h>
+
+void	sa();
+void	sb();
+void	ss();
+void	pa();
+void	pb();
+void	ra();
+void	rb();
+void	rr();
+void	rra();
+void	rrb();
+void	rrr();
+
+#endif
---
+++ push_swap_utils2.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amary <amary@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 15:10:49 by amary             #+#    #+#             */
+/*   Updated: 2025/12/10 19:38:39 by amary            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+
+void	ra(int *stack_a, int size)
+{
+
+}
+
+void	rb(int *stack_b, int size)
+{
+
+}
+
+void	rr(int *stack_a, int *stack_b, int size_a, int size_b)
+{
+	
+}
+
+void	rra(int *stack_a, int size)
+{
+
+}
+
+void	rrb(int *stack_b, int size)
+{
+	
+}

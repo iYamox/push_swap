3e70f7f89b5808beac7f559531da9bcb0628f477
Alexandre Mary <amary@f0r7s18.clusters.42paris.fr>
2025-12-15 19:33:47 +0100

    push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,26 @@
 /*   By: amary <amary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 15:10:46 by amary             #+#    #+#             */
-/*   Updated: 2025/12/15 18:10:35 by amary            ###   ########.fr       */
+/*   Updated: 2025/12/15 19:33:32 by amary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_verif(char **argv)
+{
+	
+}
+
+/* Je dois verifier que tous sois conforme :
+
+- Si c'est une chaine de charactere je dois la convertir avec split + itoa renvoyer erreur si elle est vide
+- Si y'a des signes bizarres ca doit renvoyer "erreur" donc soit --5 ++5 +-5 juste + ou - 5-2 etc...
+- Si c'est au dessus de int min ou int max ca renvoi error 
+- Pas de doublon
+- Si la liste est deja trier je dois sortir 0 argument
+- Erreur de sortie ca renvoie error*/
+
 void	ft_push_swap(void)
 {
+	
 	return ;
 }
